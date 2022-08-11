68b9b1044eda74600c64f4eb44d9c6ae67adfea0
Rhiddhi Prasad Das <rhiddhiprasad@gmail.com>
2022-08-11 14:06:36 +0530

    Update main.py
    Made it more readable and added a feature to find the biggest prime number snowball possible

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-sPoint = int(input("Enter Starting Point: "))
+import sys
 
 def isPrime(num):
-        if(num%2==0):
-                return(False)
-        for i in range(3,num//2,2):
-                if(num%i==0):
-                        return(False)
-        return(True)
+	if(num%2==0):
+		return(False)
+	for i in range(3,num//2,2):
+		if(num%i==0):
+			return(False)
+	return(True)
 
 def findSnowball(num,sba=[]):
-        sbArr=sba
-        found=False
-        for i in range(1,10,2):
-                n = int(str(num)+str(i))
-                if(isPrime(n)):
-                        found = True
-                        findSnowball(n,sbArr) 
-        if(not found):
-                sbArr.append(num) 
-        return sbArr
+	sbArr=sba
+	found=False
+	for i in range(1,10,2):
+		n = int(str(num)+str(i))
+		if(isPrime(n)):
+			found = True
+			findSnowball(n,sbArr)
+	if(not found):
+		sbArr.append(num)
+		print(num)
+	return sbArr
 
-print(findSnowball(sPoint))
+try:
+	print(findSnowball(sys.argv[1]))
+except:
+	allFound=[]
+	for i in [1,2,3,5,7]:
+		allFound+=findSnowball(i)
+	print("\n\nBiggest Snowball Prime Number Possible: ",max(allFound))
